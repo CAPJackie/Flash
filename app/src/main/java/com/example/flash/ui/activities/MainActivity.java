eb5d43aa7e239e9c2b8bfcb10bcea52c3d81d71d
Juan David Ramirez Mendoza <c4pjackie@gmail.com>
2020-11-08 17:22:20 -0500

    Settings icon added to the assets, Settings activity and basic layout finished, :wip: Transition between Main Activity and Settings Activity

--- app/src/main/java/com/example/flash/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/flash/ui/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.flash;
+package com.example.flash.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.flash.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
@@ -9,8 +11,6 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.example.flash.ui.main.SectionsPagerAdapter;
@@ -21,16 +21,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        setViewPagerConfig();
+        setActionListeners();
+    }
+
+    private void setViewPagerConfig() {
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+    }
 
-        fab.setOnClickListener(new View.OnClickListener() {
+    private void setActionListeners() {
+        FloatingActionButton settingsButton = findViewById(R.id.settings_button);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent();
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
---
+++ app/src/main/java/com/example/flash/ui/activities/SettingsActivity.java
@@ -0,0 +1,26 @@
+package com.example.flash.ui.activities;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.flash.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+}
